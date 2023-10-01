9c2e6d47250fec5d8628a2e23c80a228673f37e5
leticiabsilva03 <leticiabsilva03@gmail.com>
2023-10-01 13:37:30 -0300

    add: content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -13,6 +13,10 @@ export class SmallCardComponent implements OnInit {
   @Input()
   cardTitle: string = ""
 
+  @Input()
+  Id:string = "0"
+
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Readers' Top 66 Horror Novels of the Past Three Years",
+    "description": "For devotees of the horror genre, Halloween is the best holiday on the calendar. Once a year, like creepy clockwork, the culture at large acknowledges the best things in life and unlife: revenants, witches, haunted houses, derelict amusement parks, and nightmare fungal issues.",
+    "photoCover": "https://images.gr-assets.com/misc/1695318155-1695318155_goodreads_misc.png"
+  },
+  {
+    "id":"2",
+    "title":"Enchanting to Wicked: 36 New Witch Books to Read Now",
+    "description": "According to most scholars, the first witch to appear in Western literature is probably Circe, best known for her appearance in Homer’s Odyssey, written sometime in the 8th century BCE. Legend holds that Circe was the daughter of Hecate, goddess of magic, the moon, and, um, cattle tending. She had a large portfolio.",
+    "photoCover": "https://images.gr-assets.com/misc/1692912242-1692912242_goodreads_misc.png"
+  },
+  {
+    "id":"3",
+    "title":"64 New Books to Scratch That Romantasy Itch",
+    "description": "In the book world, the term romantasy is a notoriously slippery designation. Is it a genre? A subgenre? A hybrid? Does it matter? Also referred to with squishy terms like fantasy romance and romantic fantasy (helpful, no?), the concept refers to books that combine elements of the romance novel with the setting and tropes of fantasy literature—epic, urban, cozy, what have you. It might be a standard fantasy superstructure with a handful of super-spicy scenes and real-world swear words. It might be a more heavily romance-driven plotline dropped into a fantasy setting. Or it might just be a witchy rom-com. (We find that the covers tend to give away whether the book tilts more heavily toward the fantasy or the romance, erm, end of things.)",
+    "photoCover": "https://images.gr-assets.com/misc/1692993266-1692993266_goodreads_misc.png"
+  },
+  {
+    "id":"4",
+    "title":"54 New Books to Discover This Hispanic Heritage Month",
+    "description": "To help celebrate National Hispanic Heritage Month—which runs annually in the U.S. from September 15 to October 15—we’ve collected here 54 new and upcoming books from Latino/a/x/e authors. Each of the titles here is a 2023 publication (either on shelves now or publishing before the end of the year), and you’ll find there’s a remarkably wide array of plots to browse, investigate, and choose from. Some spotlight items, in no particular order: YA specialist and National Book Award winner Elizabeth Acevedo is back on shelves with Family Lore, her first foray into adult fiction. The very buzzy upcoming What the River Knows, from author Isabel Ibañez, features a 19th-century fantasy adventure that starts in Buenos Aires and winds up in Cairo. If you’re in the market for magical flowers, consider J.C. Cervantes’ The Enchanted Hacienda.",
+    "photoCover": "https://images.gr-assets.com/misc/1692911951-1692911951_goodreads_misc.png"
+  }
+]
